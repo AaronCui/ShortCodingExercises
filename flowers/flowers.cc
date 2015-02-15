97a3de233d60708a02418d3f20316c6182ec6295
unknown <aaroncui26@hotmail.com>
2015-02-15 01:54:13 -0800

    Improved with no nested loop

--- flowers/flowers.cc
+++ flowers/flowers.cc
@@ -9,45 +9,26 @@
 
 int main(void){
 
- 
-
-//Helpers for input and output
-
     int numFlowers, numPeople;
-    int maxPurchase;
     int total = 0; 
-    
+    std::vector<int> prices;
+	
     std::cin >> numFlowers >> numPeople;
-    int C[numFlowers];
+	int temp;
     for(int i = 0; i < numFlowers; i++){
-       std::cin >> C[i];
+       std::cin >> temp;
+	   prices.push_back(temp);
     }
-    //maxPurchase = N/K;
-    //bool extraFlag = 0; 
-    std::vector<int> prices (C, C+numFlowers);
-    std::sort(prices.begin(), prices.end(), std::greater<int>());
-    if (numFlowers % numPeople == 0)
-        maxPurchase = numFlowers/numPeople;
-    else 
-        maxPurchase = numFlowers/numPeople + 1;
-    
-    int iterator = 0;
-    for (int j = 1; j < maxPurchase + 1; j++){
-        //std::cout << "SIZE" << prices.size() << std::endl;
-        for (int k = 0; k < numPeople; k++){
-            //std::cout << "ITERATOR IS　" << iterator << std::endl;
-            if (iterator < prices.size()){
-                total += j * prices[iterator];
-                iterator++;
-            }
-            //std::cout << "Current Total " << total << std::endl;
-        }
-        
+	
+	std::sort(prices.begin(), prices.end(), std::greater<int>());
+	
+	for (int j = 0, multiplyer = 1; j < prices.size(); j++){
+		total += multiplyer * prices[j];
+		if ((j+1) % numPeople == 0)
+			multiplyer ++;   
     }
     
     std::cout << total << std::endl;
-   //int result = 100/51;
- //  cout << result << "\n";
    
    return 0;
 }
